00e2d80f92d864b93ee60192360b74b6978e9792
KelpGF <kelpgf@gmail.com>
2023-01-24 21:56:02 -0300

    chore: add SQL DB scripts

---
+++ databases/scripts/before-up.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker exec nlw-setup-db ./scripts/run-migrations.sh
---
+++ databases/scripts/run-migrations.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+mysql \
+-u"root" \
+-p"$MYSQL_ROOT_PASSWORD" \
+--execute="DROP DATABASE IF EXISTS $MYSQL_DATABASE; CREATE DATABASE $MYSQL_DATABASE; DROP DATABASE IF EXISTS $MYSQL_TESTS_DATABASE; CREATE DATABASE $MYSQL_TESTS_DATABASE;"
+
+for i in {30..0}; do
+			if echo 'SELECT 1' | mysql -u"root" -p"$MYSQL_ROOT_PASSWORD" &> /dev/null; then
+				break
+			fi
+			echo 'MySQL init process in progress...'
+			sleep 1
+		done
+
+for f in ./migrations/*.sql; do
+	echo "$0: running $f"; mysql -u"root" -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < "$f"
+	echo "$0: running $f"; mysql -u"root" -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_TESTS_DATABASE" < "$f"
+  echo ""
+  sleep 1
+done
