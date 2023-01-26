e93396d74e52bf0cf95386678d1f2b6dc0c6225c
KelpGF <kelpgf@gmail.com>
2023-01-26 19:54:45 -0300

    feat: ensure HabitSqlRepository returns a habit id on insert success

--- src/infra/db/sql/habit-control/habit/habit-sql-repository.ts
+++ src/infra/db/sql/habit-control/habit/habit-sql-repository.ts
@@ -1,8 +1,14 @@
-import { FindHabitByIdRepository, HabitModel } from '@/data/usecases/habits/db-create-habit-protocols'
+import { CreateHabitParams, FindHabitByIdRepository, HabitModel, InsertHabitRepository } from '@/data/usecases/habits/db-create-habit-protocols'
 import { SqlHelper } from '@/infra/db/sql/helpers/sql-helper'
 
-export class HabitSqlRepository implements FindHabitByIdRepository {
+export class HabitSqlRepository implements InsertHabitRepository, FindHabitByIdRepository {
   constructor (private readonly database: SqlHelper) {}
+
+  async insert (insertHabitParams: CreateHabitParams): Promise<string> {
+    const habitTable = await this.database.getTable('habits')
+    const result = await habitTable.create({ title: insertHabitParams.title })
+    return String(result.id)
+  }
 
   async findById (habitId: string): Promise<HabitModel> {
     const habitTable = await this.database.getTable('habits')
