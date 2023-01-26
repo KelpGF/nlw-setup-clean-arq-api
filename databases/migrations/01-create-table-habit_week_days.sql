1f563eb6e551ef31e92fab6b62b0e47047651866
KelpGF <kelpgf@gmail.com>
2023-01-26 19:59:07 -0300

    chore: update fk habit_week_days.habit_id

--- databases/migrations/01-create-table-habit_week_days.sql
+++ databases/migrations/01-create-table-habit_week_days.sql
@@ -2,5 +2,5 @@ CREATE TABLE habit_week_days (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     habit_id int NOT NULL,
     week_day INTEGER NOT NULL,
-    CONSTRAINT habit_week_days_habit_id_fkey FOREIGN KEY (habit_id) REFERENCES habits (id) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT habit_week_days_habit_id_fkey FOREIGN KEY (habit_id) REFERENCES habits (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
