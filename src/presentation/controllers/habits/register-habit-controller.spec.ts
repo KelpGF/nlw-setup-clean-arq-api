205737ae770aa576a0f5720b2872982f403d33ad
KelpGF <kelpgf@gmail.com>
2023-01-26 21:31:31 -0300

    test: ensure CreateHabitController returns a bad request if no title provided

--- src/presentation/controllers/habits/register-habit-controller.spec.ts
+++ src/presentation/controllers/habits/register-habit-controller.spec.ts
@@ -1,7 +1,8 @@
 import { mockCreateHabitParams } from '@/domain/tests/mock-habit'
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { mockCreateHabit } from '@/presentation/tests/mock-habit'
 import { CreateHabitController } from './register-habit-controller'
-import { ControllerRequest, CreateHabit, CreateHabitParams } from './register-habit-controller-protocols'
+import { badRequest, ControllerRequest, CreateHabit, CreateHabitParams } from './register-habit-controller-protocols'
 
 type SutTypes = {
   sut: CreateHabitController
@@ -19,6 +20,12 @@ const mockControllerRequest = (): ControllerRequest<CreateHabitParams> => ({
 })
 
 describe('CreateHabit Controller', () => {
+  test('Should return a bad request if no title provided', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle({})
+    expect(response).toEqual(badRequest(new MissingParamError('title')))
+  })
+
   test('Should call CreateHabitUseCase with correct values', async () => {
     const { sut, createHabitStub } = makeSut()
     const createSpy = jest.spyOn(createHabitStub, 'create')
