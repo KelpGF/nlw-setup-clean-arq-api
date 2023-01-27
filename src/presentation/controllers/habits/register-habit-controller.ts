4a7fd1f70dacacc817daa1211f20ebcadd5c9605
KelpGF <kelpgf@gmail.com>
2023-01-26 21:31:06 -0300

    feat: ensure CreateHabitController returns a bad request if no title provided

--- src/presentation/controllers/habits/register-habit-controller.ts
+++ src/presentation/controllers/habits/register-habit-controller.ts
@@ -1,9 +1,13 @@
-import { HabitModel, CreateHabit, CreateHabitParams, success, Controller, ControllerRequest } from './register-habit-controller-protocols'
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { HabitModel, CreateHabit, CreateHabitParams, success, Controller, ControllerRequest, badRequest } from './register-habit-controller-protocols'
 
 export class CreateHabitController implements Controller<HabitModel> {
   constructor (private readonly createHabit: CreateHabit) {}
 
   async handle (request: ControllerRequest<CreateHabitParams>) {
+    const title = request.body?.title
+    if (!title) return badRequest(new MissingParamError('title'))
+
     const habit = await this.createHabit.create(request.body as CreateHabitParams)
     return success<HabitModel>(habit)
   }
