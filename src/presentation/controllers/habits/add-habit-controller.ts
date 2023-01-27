47f53f42ada330f90fdb3c908639c5de182dcaff
KelpGF <kelpgf@gmail.com>
2023-01-26 21:40:27 -0300

    refactor: remove duplicate code

--- src/presentation/controllers/habits/add-habit-controller.ts
+++ src/presentation/controllers/habits/add-habit-controller.ts
@@ -5,10 +5,10 @@ export class AddHabitController implements Controller<HabitModel> {
   constructor (private readonly createHabit: CreateHabit) {}
 
   async handle (request: ControllerRequest<CreateHabitParams>) {
-    const title = request.body?.title
-    const weekDays = request.body?.weekDays
-    if (!title) return badRequest(new MissingParamError('title'))
-    if (!weekDays) return badRequest(new MissingParamError('weekDays'))
+    const requiredFields = ['title', 'weekDays']
+    for (const field of requiredFields) {
+      if (!request.body?.[field]) return badRequest(new MissingParamError(field))
+    }
 
     const habit = await this.createHabit.create(request.body as CreateHabitParams)
     return success<HabitModel>(habit)
