6cd750bcb59144a899a1d0fd3e266aa19abf2a0a
KelpGF <kelpgf@gmail.com>
2023-01-28 19:51:31 -0300

    test: ensure ValidationComposite returns the first error more then one validation fails

--- src/validations/validators/validation-composite.spec.ts
+++ src/validations/validators/validation-composite.spec.ts
@@ -1,3 +1,5 @@
+import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { Validation } from '@/presentation/protocols/validation'
 import { mockValidation } from '@/validations/test/mock-validation'
 import { ValidationComposite } from './validation-composite'
@@ -19,6 +21,14 @@ describe('ValidationComposite', () => {
     jest.spyOn(validations[0], 'validate').mockReturnValue(new Error())
     const error = sut.validate({})
     expect(error).toEqual(new Error())
+  })
+
+  test('Should return the first error more then one validation fails', () => {
+    const { sut, validations } = makeSut()
+    jest.spyOn(validations[0], 'validate').mockReturnValue(new MissingParamError('any_field'))
+    jest.spyOn(validations[1], 'validate').mockReturnValue(new InvalidParamError('any_param'))
+    const error = sut.validate({})
+    expect(error).toEqual(new MissingParamError('any_field'))
   })
 
   test('Should return null if validation success', () => {
