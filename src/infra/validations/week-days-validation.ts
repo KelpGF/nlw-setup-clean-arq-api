cc4f001f51b0710bbac05af9b24918a9c288d0cd
KelpGF <kelpgf@gmail.com>
2023-01-28 19:24:39 -0300

    feat: ensure WeekDaysValidationInfra returns false if repeat a weekDay

--- src/infra/validations/week-days-validation.ts
+++ src/infra/validations/week-days-validation.ts
@@ -3,6 +3,7 @@ import { WeekDaysValidator } from '@/validations/protocols/week-days-validator'
 export class WeekDaysValidation implements WeekDaysValidator {
   isValid (weekDays: number[]): boolean {
     if (weekDays.length > 7) return false
+    if (this.removeDuplicates(weekDays).length < weekDays.length) return false
     return weekDays.every(this.isValidNumber)
   }
 
@@ -10,5 +11,9 @@ export class WeekDaysValidation implements WeekDaysValidator {
     const weekDay = Number(val)
     if (Number.isNaN(weekDay)) return false
     return weekDay >= 0 && weekDay <= 6
+  }
+
+  private removeDuplicates (data: number[]) {
+    return data.filter((val, idx) => idx === data.indexOf(val))
   }
 }
