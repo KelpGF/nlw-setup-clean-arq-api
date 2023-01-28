1ba5da2acc391832547e597723600c765edfec8f
KelpGF <kelpgf@gmail.com>
2023-01-28 18:23:23 -0300

    feat: ensure RequiredFieldValidation returns a missing param error if invalid value provided

--- src/validations/validators/required-field-validation.ts
+++ src/validations/validators/required-field-validation.ts
@@ -1,10 +1,17 @@
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { Validation } from '@/presentation/protocols/validation'
 
-export class RequiredFieldValidation implements Validation<Record<string, any>> {
+export class RequiredFieldValidation implements Validation<Record<any, any>> {
   constructor (private readonly fieldName: string) {}
 
-  validate (input: Record<string, any>): Error | null {
-    return !Reflect.has(input, this.fieldName) ? new MissingParamError(this.fieldName) : null
+  validate (input: Record<any, any>): Error | null {
+    const error = new MissingParamError(this.fieldName)
+
+    try {
+      const isValid = Reflect.has(input, this.fieldName)
+      return isValid ? null : error
+    } catch {
+      return error
+    }
   }
 }
