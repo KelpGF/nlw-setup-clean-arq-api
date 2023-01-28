040c2b6342b20d2c42bbe411bba6a751195cfd3b
KelpGF <kelpgf@gmail.com>
2023-01-28 18:12:48 -0300

    test: ensure RequiredFieldValidation returns a missing param error if validation fails

---
+++ src/validations/validators/required-field-validation.spec.ts
@@ -0,0 +1,12 @@
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { RequiredFieldValidation } from './required-field-validation'
+
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation('any_field')
+
+describe('RequiredField Validation', () => {
+  test('Should return a missing param error if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({})
+    expect(error).toEqual(new MissingParamError('any_field'))
+  })
+})
