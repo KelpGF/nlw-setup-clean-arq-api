be1177fa42da7a2430b0e729ecd2f87f19bc7bfd
KelpGF <kelpgf@gmail.com>
2023-01-28 20:07:32 -0300

    test: ensure CreateHabitController validate body with a validator

--- src/presentation/controllers/habits/add-habit-controller.spec.ts
+++ src/presentation/controllers/habits/add-habit-controller.spec.ts
@@ -1,18 +1,22 @@
 import { mockCreateHabitParams } from '@/domain/tests/mock-habit'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { Validation } from '@/presentation/protocols/validation'
 import { mockCreateHabit } from '@/presentation/tests/mock-habit'
+import { mockValidation } from '@/presentation/tests/mock-validation'
 import { AddHabitController } from './add-habit-controller'
 import { badRequest, Controller, CreateHabit, CreateHabitParams, serverError } from './add-habit-controller-protocols'
 
 type SutTypes = {
   sut: AddHabitController
   createHabitStub: CreateHabit
+  validationStub: Validation
 }
 
 const makeSut = (): SutTypes => {
   const createHabitStub = mockCreateHabit()
-  const sut = new AddHabitController(createHabitStub)
-  return { sut, createHabitStub }
+  const validationStub = mockValidation()
+  const sut = new AddHabitController(createHabitStub, validationStub)
+  return { sut, createHabitStub, validationStub }
 }
 
 const mockControllerRequest = (): Controller.Request<CreateHabitParams> => ({
@@ -20,18 +24,6 @@ const mockControllerRequest = (): Controller.Request<CreateHabitParams> => ({
 })
 
 describe('CreateHabit Controller', () => {
-  test('Should return a bad request if no title provided', async () => {
-    const { sut } = makeSut()
-    const response = await sut.handle({})
-    expect(response).toEqual(badRequest(new MissingParamError('title')))
-  })
-
-  test('Should return a bad request if no weekDays provided', async () => {
-    const { sut } = makeSut()
-    const response = await sut.handle({ body: { title: 'any_title', weekDays: undefined as unknown as number[] } })
-    expect(response).toEqual(badRequest(new MissingParamError('weekDays')))
-  })
-
   test('Should call CreateHabitUseCase with correct values', async () => {
     const { sut, createHabitStub } = makeSut()
     const createSpy = jest.spyOn(createHabitStub, 'create')
@@ -45,5 +37,30 @@ describe('CreateHabit Controller', () => {
     jest.spyOn(createHabitStub, 'create').mockRejectedValueOnce(new Error())
     const response = await sut.handle(mockControllerRequest())
     expect(response).toEqual(serverError(new Error()))
+  })
+
+  test('Should call Validation with correct value', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    const httpRequest = mockControllerRequest()
+
+    await sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return a bad request if Validation return an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(mockControllerRequest())
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
+
+  test('Should return a server error if Validation throws', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockImplementationOnce(() => { throw new Error() })
+    const httpResponse = await sut.handle(mockControllerRequest())
+
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
