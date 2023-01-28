bdb3afd512efc5a85ac648dbfc0679c995281473
KelpGF <kelpgf@gmail.com>
2023-01-28 19:14:49 -0300

    test: ensure WeekDaysValidationInfra returns false if a invalid weekDays length

--- src/infra/validations/week-days-validation.spec.ts
+++ src/infra/validations/week-days-validation.spec.ts
@@ -8,4 +8,10 @@ describe('WeekDays Validation', () => {
     const isValid = sut.isValid([9, 2])
     expect(isValid).toBe(false)
   })
+
+  test('Should return false if a invalid weekDays length', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid([0, 1, 2, 3, 4, 5, 6, 7])
+    expect(isValid).toBe(false)
+  })
 })
