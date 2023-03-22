ff1caba2e6d9960c09d253dc7c02cc894fcc753c
Kelvin Gomes <kelvin.gomes@tallos.com.br>
2023-03-22 19:41:33 -0300

    refactor: move ArrayRemoveDuplicates to a util method

--- src/infra/validations/week-days-validation.ts
+++ src/infra/validations/week-days-validation.ts
@@ -1,18 +1,15 @@
+import { ArrayRemoveDuplicates } from '@/shared/utils/array-util'
 import { WeekDaysValidator } from '@/validations/protocols/week-days-validator'
 
 export class WeekDaysValidation implements WeekDaysValidator {
   isValid (weekDays: number[]): boolean {
     if (weekDays.length > 7) return false
-    if (this.removeDuplicates(weekDays).length < weekDays.length) return false
+    if (ArrayRemoveDuplicates(weekDays).length < weekDays.length) return false
     return weekDays.every(this.isValidNumber)
   }
 
   private isValidNumber (weekDay: number): boolean {
     if (!Number.isInteger(weekDay)) return false
     return weekDay >= 0 && weekDay <= 6
-  }
-
-  private removeDuplicates (data: number[]) {
-    return data.filter((val, idx) => idx === data.indexOf(val))
   }
 }
---
+++ src/shared/utils/array-util.ts
@@ -0,0 +1,1 @@
+export const ArrayRemoveDuplicates = (arr: Array<number | string>): Array<number | string> => Array.from(new Set(arr))
