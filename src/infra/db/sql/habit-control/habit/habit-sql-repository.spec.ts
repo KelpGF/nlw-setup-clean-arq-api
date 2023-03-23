aca8b98567c5c03b0a300fbb46c1b6b3fa329b90
Kelvin Gomes <kelvin.gomes@tallos.com.br>
2023-03-23 09:36:08 -0300

    refactor: improve the Habit Sql Repository test suit

--- src/infra/db/sql/habit-control/habit/habit-sql-repository.spec.ts
+++ src/infra/db/sql/habit-control/habit/habit-sql-repository.spec.ts
@@ -27,25 +27,27 @@ describe('Habit Sql Repository', () => {
   test('Should return a habit model on findById success', async () => {
     const sut = makeSut()
     const createHabitParams = mockCreateHabitParams()
-    const habitId = await sut.insert(createHabitParams)
+    const { id: habitId } = await HabitTableModel.create(createHabitParams)
+    createHabitParams.weekDays.forEach(async (weekDay) => {
+      await HabitWeekDaysTableModel.create({ habit_id: habitId, week_day: weekDay })
+    })
     const habit = await sut.findById(habitId)
     expect(habit).toBeTruthy()
     expect(habit.id).toBeTruthy()
-    expect(habit.title).toBe(mockCreateHabitParams().title)
-    expect(habit.weekDays.length).toBe(createHabitParams.weekDays.length)
+    expect(habit.title).toBe(createHabitParams.title)
     expect(habit.weekDays).toEqual(createHabitParams.weekDays)
   })
 
-  test('Should create a habit and yours week days on insert success', async () => {
+  test('Should create a habit and your week days on insert success', async () => {
     const sut = makeSut()
     const createHabitParams = mockCreateHabitParams()
     const habitId = await sut.insert(createHabitParams)
-    const habits = await HabitTableModel.findAll()
+    const habit = await HabitTableModel.findByPk(habitId) as HabitTableModel
     const habitWeekDays = await HabitWeekDaysTableModel.findAll({ where: { habit_id: Number(habitId) } })
-    expect(habits.length).toBe(1)
-    expect(String(habits[0].id)).toBe(habitId)
-    expect(habits[0].title).toBe(createHabitParams.title)
-    expect(habitWeekDays.length).toBe(createHabitParams.weekDays.length)
+    expect(habit).toBeTruthy()
+    expect(habit.id).toBeTruthy()
+    expect(habit.id).toBe(Number(habitId))
+    expect(habit.title).toBe(createHabitParams.title)
     expect(habitWeekDays.map((weekDayData) => ({ habit_id: weekDayData.habit_id, week_day: weekDayData.week_day })))
       .toEqual(createHabitParams.weekDays.map((weekDay) => ({ habit_id: Number(habitId), week_day: weekDay })))
   })
