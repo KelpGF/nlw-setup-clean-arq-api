144962ad79e4afbe4059082807720187a7cd3af6
Kelvin Gomes <kelvin.gomes@tallos.com.br>
2023-03-23 02:25:56 -0300

    fix: remove unused files

--- src/infra/db/sql/habit-control/connection.spec.ts
+++
@@ -1,9 +0,0 @@
-import { dbSqlHabitConnection } from './connection'
-
-describe('dbSqlHabitConnection Connection', () => {
-  test('Should database has been initialized', async () => {
-    const sut = dbSqlHabitConnection
-    const result = await sut.authenticate()  // eslint-disable-line
-    expect(result).toBeUndefined()
-  })
-})
