3504b80893c274b1e882398edf0a0fbe07fff532
Kelvin Gomes <kelvin.gomes@tallos.com.br>
2023-03-22 23:56:51 -0300

    test: ensure SequelizeInitDatabase returns a new Sequelize

--- src/infra/db/sql/helpers/sequelize-helper.spec.ts
+++ src/infra/db/sql/helpers/sequelize-helper.spec.ts
@@ -1,11 +1,17 @@
 import env from '@/main/config/env'
 import { Sequelize } from 'sequelize'
-import { SequelizeNewConnection } from './sequelize-helper'
+import { SequelizeInitDatabase, SequelizeNewConnection } from './sequelize-helper'
 
 describe('Sequelize Helper', () => {
   test('Should SequelizeNewConnection return a new Sequelize', () => {
     const sut = SequelizeNewConnection
     const result = sut(env.HABIT_CONTROL_TEST_DB_CONNECTION_STRING)
+    expect(result).toBeInstanceOf(Sequelize)
+  })
+
+  test('Should SequelizeInitDatabase return a new Sequelize', () => {
+    const sut = SequelizeInitDatabase
+    const result = sut(env.HABIT_CONTROL_TEST_DB_CONNECTION_STRING, [])
     expect(result).toBeInstanceOf(Sequelize)
   })
 })
