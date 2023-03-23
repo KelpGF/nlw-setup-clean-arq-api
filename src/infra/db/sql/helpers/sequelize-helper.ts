ad13be61e5fa8334918826a9a71a5153eb696b60
Kelvin Gomes <kelvin.gomes@tallos.com.br>
2023-03-22 23:56:31 -0300

    feat: ensure SequelizeInitDatabase returns a new Sequelize

--- src/infra/db/sql/helpers/sequelize-helper.ts
+++ src/infra/db/sql/helpers/sequelize-helper.ts
@@ -1,3 +1,10 @@
 import { Sequelize } from 'sequelize'
+import { FnGenerateModel } from '../habit-control/models/types'
 
 export const SequelizeNewConnection = (uri: string): Sequelize => new Sequelize(uri, { dialect: 'mysql', logging: false })
+
+export const SequelizeInitDatabase = (url: string, modelsGenerator: FnGenerateModel[]): Sequelize => {
+  const db = SequelizeNewConnection(url)
+  modelsGenerator.forEach((fn) => { fn(db) })
+  return db
+}
