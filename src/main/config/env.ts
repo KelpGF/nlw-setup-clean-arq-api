be361f151541f992214132d8787f4f7c19055125
Kelvin Gomes <kelvin.gomes@tallos.com.br>
2023-03-22 23:53:28 -0300

    chore: add db connections string in env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,5 @@
 export default {
-  api_port: Number(process.env.API_PORT) || 3333
+  api_port: Number(process.env.API_PORT) || 3333,
+  HABIT_CONTROL_DB_CONNECTION_STRING: process.env.HABIT_CONTROL_DB_CONNECTION_STRING || 'mysql://root:password@localhost:3306/habits_control',
+  HABIT_CONTROL_TEST_DB_CONNECTION_STRING: process.env.HABIT_CONTROL_DB_CONNECTION_STRING || 'mysql://root:password@localhost:3306/habits_control_test'
 }
